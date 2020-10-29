4d192ceb1b84b1280c29dd655606b64020be6a85
mateomeli <mateomeli7@gmail.com>
2020-10-29 18:12:05 -0300

    TP3

---
+++ Meli.Mateo.2°C.TP3/Clases Instanciables/Profesor.cs
@@ -0,0 +1,13 @@
+﻿using EntidadesAbstractas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_Instanciables
+{
+    public sealed class Profesor : Universitario
+    {
+    }
+}
